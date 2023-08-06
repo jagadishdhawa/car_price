38e8b87ec4fdb834bcd605613d9aa25a6dc43e15
Jagadish Dhawa <126097558+jagadishdhawa@users.noreply.github.com>
2023-08-06 14:12:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def predict_price(year, km_driven, fuel, seller_type, transmission, brand, owner
     input_data = preprocess_data(input_data)
 
 
-    prediction = model.predict(input_data)[0]
+    prediction = model.predict(input_data)
     return prediction
 
 
